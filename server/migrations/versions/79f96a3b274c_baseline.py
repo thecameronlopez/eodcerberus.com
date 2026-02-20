4357455962320272ca53bb2c699e6296f8a2252e
thecameronlopez <kamrin717@gmail.com>
2026-02-20 11:54:32 -0600

    migration setup

---
+++ server/migrations/versions/79f96a3b274c_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: 79f96a3b274c
+Revises: 
+Create Date: 2026-02-20 11:53:18.366043
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '79f96a3b274c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
